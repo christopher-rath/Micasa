2c4f86597b4b2e7647f11ce87dba669c2427f105
Christopher Rath <christopher.rath@isg-one.com>
2022-04-26 16:50:14 -0400

    Update copyright string & release version number.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,7 +2,7 @@
 // Micasa -- Your Photo Home -- A lightweight photo organiser & editor.
 // Author: Christopher Rath <christopher@rath.ca>
 // Archived at: http://rath.ca/
-// Copyright 2021 © Christopher Rath
+// Copyright 2021-2022 © Christopher Rath
 // Distributed under the GNU Lesser General Public License v2.1
 //     (see the About–→Terms menu item for the license text).
 // Warranty: None, see the license.
@@ -984,9 +984,9 @@ namespace Micasa
         // Micasa About panel.  This section is at the end of the file to make it 
         // easy to find, to update the version information.
         public const string sMcMainSection = @"Main";
-        public const string sMcVersion = @"0.03";
+        public const string sMcVersion = @"0.04";
         public const string sMcPlatform = @"Windows 10";
-        public const string sMcCopyright = @"2022";
+        public const string sMcCopyright = @"2021{\'96}2022"; // "{\'96}" is an RTF en dash character.
         public const string sVersionToRepl = @"[@Version String@]";
         public const string sPlatformToRepl = @"[@Platform String@]";
         public const string sCopyrightToRepl = @"[@Copyright Date@]";
