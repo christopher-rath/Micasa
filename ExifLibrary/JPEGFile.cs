7d7b20eff2f984af9becf28078e6ee0d1e00ab5d
Christopher Rath <christopher.rath@isg-one.com>
2023-03-06 22:14:26 -0500

    Suppress warnings due to ExifLibrary being targeted for an older version of .NET.

--- ExifLibrary/JPEGFile.cs
+++ ExifLibrary/JPEGFile.cs
@@ -5,6 +5,23 @@ using System.Text;
 
 namespace ExifLibrary
 {
+#pragma warning disable CA1036 // Override methods on comparable types
+#pragma warning disable CA1051 // Do not declare visible instance fields
+#pragma warning disable CA1305 // Specify IFormatProvider
+#pragma warning disable CA1507 // Use nameof to express symbol names
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+#pragma warning disable CA1710 // Identifiers should have correct suffix
+#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
+#pragma warning disable CA1715 // Identifiers should have correct prefix
+#pragma warning disable CA1720 // Identifier contains type name
+#pragma warning disable CA1725 // Parameter names should match base declaration
+#pragma warning disable CA1805 // Do not initialize unnecessarily
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable CA1825 // Avoid zero-length array allocations
+#pragma warning disable CA1854 // Prefer the 'IDictionary.TryGetValue(TKey, out TValue)' method
+#pragma warning disable CA2251 // Use 'string.Equals'
+#pragma warning disable IDE0090 // Use 'new(...)'
+#pragma warning disable IDE1006 // Naming Styles
     /// <summary>
     /// Represents the binary view of a JPEG compressed file.
     /// </summary>
@@ -955,7 +972,7 @@ namespace ExifLibrary
             if (ifd.ContainsKey(ExifTag.MakerNote))
                 fieldqueue.Enqueue(ifd[ExifTag.MakerNote]);
 
-            // Offset to start of field data from start of TIFF header
+                // Offset to start of field data from start of TIFF header
             uint dataoffset = (uint)(2 + ifd.Count * 12 + 4 + stream.Position - tiffoffset);
             uint currentdataoffset = dataoffset;
             long absolutedataoffset = stream.Position + (2 + ifd.Count * 12 + 4);
--- ExifLibrary/MathEx.cs
+++ ExifLibrary/MathEx.cs
@@ -3,6 +3,24 @@ using System.Text;
 
 namespace ExifLibrary
 {
+#pragma warning disable CA1036 // Override methods on comparable types
+#pragma warning disable CA1051 // Do not declare visible instance fields
+#pragma warning disable CA1305 // Specify IFormatProvider
+#pragma warning disable CA1507 // Use nameof to express symbol names
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+#pragma warning disable CA1710 // Identifiers should have correct suffix
+#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
+#pragma warning disable CA1715 // Identifiers should have correct prefix
+#pragma warning disable CA1720 // Identifier contains type name
+#pragma warning disable CA1725 // Parameter names should match base declaration
+#pragma warning disable CA1805 // Do not initialize unnecessarily
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable CA1825 // Avoid zero-length array allocations
+#pragma warning disable CA1830 // Prefer strongly-typed Append and Insert method overloads on StringBuilder
+#pragma warning disable CA1854 // Prefer the 'IDictionary.TryGetValue(TKey, out TValue)' method
+#pragma warning disable CA2251 // Use 'string.Equals'
+#pragma warning disable IDE0090 // Use 'new(...)'
+#pragma warning disable IDE1006 // Naming Styles
     /// <summary>
     /// Contains extended Math functions.
     /// </summary>
@@ -75,7 +93,7 @@ namespace ExifLibrary
         /// Represents a generic rational number represented by 32-bit signed numerator and denominator.
         /// </summary>
         public struct Fraction32 : IComparable, IFormattable, IComparable<Fraction32>, IEquatable<Fraction32>
-        {
+       {
             #region Constants
             private const uint MaximumIterations = 10000000;
             #endregion
