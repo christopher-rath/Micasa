524779f457e1a446363f26b03b3c2e1165c84d16
Christopher Rath <christopher.rath@isg-one.com>
2023-10-22 21:44:12 -0400

    Cleared Intellicode suggestions.

--- ExifLibrary/ExifExtendedProperty.cs
+++ ExifLibrary/ExifExtendedProperty.cs
@@ -3,23 +3,9 @@ using System.Text;
 
 namespace ExifLibrary
 {
-#pragma warning disable CA1036 // Override methods on comparable types
 #pragma warning disable CA1051 // Do not declare visible instance fields
 #pragma warning disable CA1305 // Specify IFormatProvider
-#pragma warning disable CA1507 // Use nameof to express symbol names
-#pragma warning disable CA1707 // Identifiers should not contain underscores
-#pragma warning disable CA1710 // Identifiers should have correct suffix
-#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
-#pragma warning disable CA1715 // Identifiers should have correct prefix
-#pragma warning disable CA1720 // Identifier contains type name
-#pragma warning disable CA1725 // Parameter names should match base declaration
-#pragma warning disable CA1805 // Do not initialize unnecessarily
-#pragma warning disable CA1822 // Mark members as static
-#pragma warning disable CA1825 // Avoid zero-length array allocations
-#pragma warning disable CA1854 // Prefer the 'IDictionary.TryGetValue(TKey, out TValue)' method
-#pragma warning disable CA2251 // Use 'string.Equals'
 #pragma warning disable IDE0090 // Use 'new(...)'
-#pragma warning disable IDE1006 // Naming Styles
     /// <summary>
     /// Represents an enumerated value.
     /// </summary>
@@ -202,13 +188,13 @@ namespace ExifLibrary
     {
         protected string mValue;
         protected override object _Value { get { return Value; } set { Value = (string)value; } }
-        public new string Value { get { return mValue; } set { mValue = value.Substring(0, 4); } }
+        public new string Value { get { return mValue; } set { mValue = value[..4]; } }
 
         public ExifVersion(ExifTag tag, string value)
             : base(tag)
         {
             if (value.Length > 4)
-                mValue = value.Substring(0, 4);
+                mValue = value[..4];
             else if (value.Length < 4)
                 mValue = value + new string(' ', 4 - value.Length);
             else
